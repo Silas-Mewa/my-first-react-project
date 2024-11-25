8b84a984e6a57492753a9e3d6a63b9b71b9386f8
Silas-Mewa <silas.boecher@mewa.de>
2024-11-25 11:52:09 +0100

    playwright dev server CI tests

--- tests/pageLoads.spec.ts
+++ tests/pageLoads.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/' , {waitUntil: "commit"});
+  await page.goto('http://127.0.0.1:5173/' , {waitUntil: "commit"});
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("First Steps with React");
 });
 
 test('no network issues', async ({ page }) => {
-  await page.goto('http://localhost:5173/', {waitUntil: "load", timeout: 10_000});
+  await page.goto('http://127.0.0.1:5173/', {waitUntil: "load", timeout: 10_000});
 });
